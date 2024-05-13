0ba09a54d21a1c6c085e825b0c0a294be5a3ceec
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-13 14:18:12 -0400

    first two questions done

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,19 @@
-select * FROM Artist;
+-- select * FROM Track;
+
+
+-- SELECT
+--     Track.Name AS Track, 
+--       Album.Title AS Album
+-- FROM 
+--     Track JOIN Album
+-- ON
+--     Track.AlbumId = Album.AlbumId
+-- WHERE
+--     Album.Title = 'Nevermind';
+
+-- This pulls up a table with two columns, track and 
+-- album, and it displays all of the tracks on the 
+-- album 'Nevermind'
+
+------------------------------------------------------
+
