956187328805a123b6698eb638e3303a21b4b02e
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-13 14:38:17 -0400

    first few questions done

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,6 +1,6 @@
 -- select * FROM Track;
 
-
+-- Get track listings for a single album
 -- SELECT
 --     Track.Name AS Track, 
 --       Album.Title AS Album
@@ -17,3 +17,55 @@
 
 ------------------------------------------------------
 
+-- Show only columns we want to show 
+
+-- SELECT 
+--     Track.Name, Album.Title, Track.GenreId 
+-- FROM 
+--     Track 
+-- JOIN 
+--     Album ON Track.AlbumId = Album.AlbumId
+-- -- WHERE 
+-- --     Album.Title = 'Nevermind';
+-- This pulled up the name album and genre type for
+-- all the songs on the album Nevermind
+
+----------------------------------------------------
+-- Get Track Listings for all albums by Nirvana 
+
+
+-- SELECT 
+--     Track.Name, Album.Title, Artist.Name, Genre.Name 
+-- FROM 
+--     Track 
+-- JOIN 
+--     Album ON Track.AlbumId = Album.AlbumId,
+--     Artist ON Album.ArtistId = Artist.ArtistId,
+--     Genre ON Track.GenreId = Genre.GenreId
+-- WHERE 
+--     Artist.Name = 'Nirvana';
+
+-- This pulls up a table that displays in ordeR: track 
+-- name, album title, artist name, and genre name; It 
+-- is only pulling things that have an artist name 'Nirvana'
+
+-----------------------------------------------------
+-- Three columns are named 'Name'; Assign aliases to the
+-- SELECT columns so they're unambiguous 
+
+-- SELECT 
+--     Track.Name AS Track, 
+--     Album.Title AS Album, 
+--     Artist.Name AS Artist, 
+--     Genre.Name AS Genre
+-- FROM 
+--     Track 
+-- JOIN 
+--     Album ON Track.AlbumId = Album.AlbumId,
+--     Artist ON Album.ArtistId = Artist.ArtistId,
+--     Genre ON Track.GenreId = Genre.GenreId
+-- WHERE 
+--     Artist.Name = 'Nirvana';
+
+-- This just changes the names of the columns at top
+-----------------------------------------------------
